4332f55962fd1f3e012bc5612504ea3984fbc3d7
chloe-cho <chloehcho@gmail.com>
2023-09-14 22:48:04 -0500

    navigation bar

---
+++ portfolio-v2/src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home: React.FC = () => {
+  return (
+        <div className='w-full h-screen bg-[#000000] text-white'> 
+       
+        </div>
+  );
+}
+
+export default Home
